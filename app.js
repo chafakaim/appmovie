f8df6277011576ce212167bdff4fc52c2d04aa66
unknown <chafikaim@gmail.com>
2022-03-17 21:20:23 +0100

    modifier le code js et appele a une api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // appele une api avec les promess
 const key='ca451e264da44b3b907051f63fa549e8';
 let i=1;
+let container=document.querySelector('.container')
 let tab=[];
 let clear=setInterval(() => {
     let res=fetch(`https://api.themoviedb.org/3/movie/${550+i}?api_key=${key}`).then(res=>{
@@ -12,10 +13,9 @@ let clear=setInterval(() => {
         i++;
         if(tab.length === 3){
             clearInterval(clear);
-           tab.forEach(e => {
-               console.log(e);
-               
-           });
-
+            tab.forEach(e => {
+                console.log(e.original_title);
+                
+            });
         }
 }, 1000);
