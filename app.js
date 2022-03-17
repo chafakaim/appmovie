737441afdeea616bd84b7fa1dd89c23af34bfb04
unknown <chafikaim@gmail.com>
2022-03-17 20:42:31 +0100

    ajout des dossier css et de la nav bar de application

---
+++ app.js
@@ -0,0 +1,21 @@
+// appele une api avec les promess
+const key='ca451e264da44b3b907051f63fa549e8';
+let i=1;
+let tab=[];
+let clear=setInterval(() => {
+    let res=fetch(`https://api.themoviedb.org/3/movie/${550+i}?api_key=${key}`).then(res=>{
+        return res.json()
+        }).then(data=>{
+            tab.push(data);
+            console.log(tab);
+     })
+        i++;
+        if(tab.length === 3){
+            clearInterval(clear);
+           tab.forEach(e => {
+               console.log(e);
+               
+           });
+
+        }
+}, 1000);
