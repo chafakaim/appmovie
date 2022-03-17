ad837f21b71d2f63e99bb1dc9b52f612458dd626
unknown <chafikaim@gmail.com>
2022-03-17 21:49:43 +0100

    ajout des box dans le container via js

--- app.js
+++ app.js
@@ -8,14 +8,28 @@ let clear=setInterval(() => {
         return res.json()
         }).then(data=>{
             tab.push(data);
-            console.log(tab);
      })
         i++;
-        if(tab.length === 3){
+        if(tab.length === 10){
             clearInterval(clear);
             tab.forEach(e => {
-                console.log(e.original_title);
+                let div=document.createElement('div')
+                div.className='box';
+                let image=document.createElement('div')
+                image.className='image';
+                let img=document.createElement('img')
+                img.setAttribute('src',e.backdrop_path);
+                let p=document.createElement('p')
+                p.innerHTML=e.overview;
+
+                image.appendChild(img);
+                div.appendChild(image);
                 
+                let title =document.createElement('h2')
+                title.innerHTML=e.original_title;
+                div.appendChild(title);
+                div.appendChild(p);
+                container.appendChild(div);
             });
         }
 }, 1000);
