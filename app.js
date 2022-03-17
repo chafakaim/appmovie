272dcf872a5c1336d2560ddb04ba37fcd82e1568
unknown <chafikaim@gmail.com>
2022-03-17 22:10:56 +0100

    ajout des photo dans mon application et realisation d un loder on svg

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // appele une api avec les promess
 const key='ca451e264da44b3b907051f63fa549e8';
 let i=1;
+let url='https://image.tmdb.org/t/p/w500'
 let container=document.querySelector('.container')
 let tab=[];
 let clear=setInterval(() => {
@@ -9,27 +10,31 @@ let clear=setInterval(() => {
         }).then(data=>{
             tab.push(data);
      })
-        i++;
-        if(tab.length === 10){
+        i=i+2;
+        if(tab.length === 30){
             clearInterval(clear);
+            console.log(tab)
             tab.forEach(e => {
-                let div=document.createElement('div')
-                div.className='box';
-                let image=document.createElement('div')
-                image.className='image';
-                let img=document.createElement('img')
-                img.setAttribute('src',e.backdrop_path);
-                let p=document.createElement('p')
-                p.innerHTML=e.overview;
-
-                image.appendChild(img);
-                div.appendChild(image);
+                if(!e.status_message){
+                    let div=document.createElement('div')
+                    div.className='box';
+                    let image=document.createElement('div')
+                    image.className='image';
+                    let img=document.createElement('img')
+                    img.setAttribute('src',url+e.backdrop_path);
+                    let p=document.createElement('p')
+                    p.innerHTML=e.overview;
+    
+                    image.appendChild(img);
+                    div.appendChild(image);
+                    
+                    let title =document.createElement('h2')
+                    title.innerHTML=e.original_title;
+                    div.appendChild(title);
+                    div.appendChild(p);
+                    container.appendChild(div);
+                }
                 
-                let title =document.createElement('h2')
-                title.innerHTML=e.original_title;
-                div.appendChild(title);
-                div.appendChild(p);
-                container.appendChild(div);
             });
         }
 }, 1000);
