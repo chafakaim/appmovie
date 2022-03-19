5c7031a80b64260922cc3aafa26779ec5fb175ca
unknown <chafikaim@gmail.com>
2022-03-19 11:05:31 +0100

    ajout de la class api dans l application

--- api.js
+++ api.js
@@ -1,36 +1,57 @@
 class Apimovie{
-    constructor(container,keyapi,url){
+    constructor(container,keyapi,load){
         this.keyapi=keyapi;
+        this.load=load;
         this.container=container;
-        this.url=url;
         this.tab=[];
+        this.lodding=true;
         // methode qui va faire la requete a l'api
-        this.fetchapi(this.string);
+        this.fetchapi(this.keyapi,this.tab);
         // appele de la methode de display des donner dans l'application
-        this.display(this.tab,this.container);
+
+        //   fade du lodding
+      
+
+
     }
+
     /**
      * 
      * @param {string} key  
      */
-    fetchapi=function(key){
+    fetchapi=function(key,tab){
         let i=1;
         let clear=setInterval(() => {
-            fetch(`https://api.themoviedb.org/3/movie/${550+i}?api_key=${key}&?language=FR`)
+            fetch(`https://api.themoviedb.org/3/movie/${550+i}?api_key=${key}`)
             .then(res=>{
                 return res.json();
             })
             .then(data=>{
-            this.tab.push(data);
+            tab.push(data);
             })
-
+              console.log(tab)
             // clean du set intervale a une longeur volue du this.tab
-            if(this.tab.length === 50){
+            if(tab.length >= 50){
                 clearInterval(clear);
+                this.lodding=false;
+                this.display(tab,this.container);
+                   
+                // gestion du lodeur de la page
+                if(this.lodding === false){
+                    load.classList.add('fade');
+                    load.addEventListener('transitionend',function(){
+                       if(this.classList.contains('fade')){
+                           this.classList.add('in');
+                       }
+                    })
+                    }
             }
+            i++;
         }, 100);
-        return this.tab;
-    }
+        if(tab.length === 50){
+            return this.tab;
+        }
+    }.bind(this);
 
     /**
      * @param {array} tab
@@ -39,12 +60,30 @@ class Apimovie{
     display=function(tab,container){
         const urls='https://image.tmdb.org/t/p/w500';
         tab.forEach(e => {
-        container.innerHTML=`<div class="box">
-       <div class="image"><img src=${urls+e.backdrop_path}></div>
-       <h2 class="title">${e.title}</h2>
-       <p class="logo">${e.overview}</p>
-       </div>`      
+            if(!e.status_message){
+                let div=document.createElement('div')
+                div.className='box';
+                let image=document.createElement('div')
+                image.className='image';
+                let img=document.createElement('img')
+                img.setAttribute('src',urls+e.backdrop_path);
+                let p=document.createElement('p')
+                p.innerHTML=e.overview;
+
+                image.appendChild(img);
+                div.appendChild(image);
+                
+                let title =document.createElement('h2')
+                title.innerHTML=e.title;
+                div.appendChild(title);
+                div.appendChild(p);
+                container.appendChild(div);
+            }
         });
       
     }.bind(this);
 }
+const key='ca451e264da44b3b907051f63fa549e8';
+let container= document.querySelector('.container');
+let load=document.querySelector('.lodding');
+let apimovie=new Apimovie(container,key,load);
