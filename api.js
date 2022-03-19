492570a8bfaa7ce9d862874f4691e14b5150a5e4
unknown <chafikaim@gmail.com>
2022-03-19 11:21:04 +0100

    probleme ou niveau de la recherche dans le js

--- api.js
+++ api.js
@@ -1,7 +1,8 @@
 class Apimovie{
-    constructor(container,keyapi,load){
+    constructor(container,keyapi,load,form){
         this.keyapi=keyapi;
         this.load=load;
+        this.form=form;
         this.container=container;
         this.tab=[];
         this.lodding=true;
@@ -9,12 +10,12 @@ class Apimovie{
         this.fetchapi(this.keyapi,this.tab);
         // appele de la methode de display des donner dans l'application
 
-        //   fade du lodding
-      
-
-
-    }
-
+        //   getion de la recherche dans l'application
+          this.form.addEventListener('submit',(e)=>{
+              e.preventDefault();
+            this.serch(this.tab,this.container,this.form);
+          })
+        }
     /**
      * 
      * @param {string} key  
@@ -82,8 +83,52 @@ class Apimovie{
         });
       
     }.bind(this);
-}
-const key='ca451e264da44b3b907051f63fa549e8';
+
+    serch=function(value,tab,container,form){
+        {
+                let input=form.querySelector('input');
+                Array.from(container.children).forEach(e=>{
+                    container.removeChild(e);
+                })
+
+                tab=tab.filter(ele=>{
+                    if(ele.title !== undefined){
+                        let string=form[0][0].value.charAt(0).toUpperCase()+form[0][0].value.slice(1,form[0][0].value.length); 
+                      if(ele.title.indexOf(string) !== -1){
+                          console.log(ele);
+                          return ele;
+                      }
+                     }
+                })
+                tab.forEach(e => {
+                    if(!e.status_message){
+                        let div=document.createElement('div')
+                        div.className='box';
+                        let image=document.createElement('div')
+                        image.className='image';
+                        let img=document.createElement('img')
+                        img.setAttribute('src',urls+e.backdrop_path);
+                        let p=document.createElement('p')
+                        p.innerHTML=e.overview;
+        
+                        image.appendChild(img);
+                        div.appendChild(image);
+                        
+                        let title =document.createElement('h2')
+                        title.innerHTML=e.title;
+                        div.appendChild(title);
+                        div.appendChild(p);
+                        container.appendChild(div);
+                    }
+                });
+      }     
+
+
+    }.bind(this);
+};
+let key='ca451e264da44b3b907051f63fa549e8';
 let container= document.querySelector('.container');
+let form=document.querySelector('form')
 let load=document.querySelector('.lodding');
-let apimovie=new Apimovie(container,key,load);
+let input=document.querySelector('input[type="text"]');
+let apimovie=new Apimovie(container,key,load,form);
